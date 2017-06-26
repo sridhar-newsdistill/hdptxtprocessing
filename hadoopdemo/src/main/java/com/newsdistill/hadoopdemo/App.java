db2a14d61bf8a45addee32ca52ae4ac7c55cf856
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2017-06-26 14:27:49 +0530

    hadoop client dependancy added

--- hadoopdemo/src/main/java/com/newsdistill/hadoopdemo/App.java
+++ hadoopdemo/src/main/java/com/newsdistill/hadoopdemo/App.java
@@ -1,11 +1,18 @@
 package com.newsdistill.hadoopdemo;
 
+import java.net.URL;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+  static{
+    URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+  }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
